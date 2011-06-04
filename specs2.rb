f974ee9ab9c653c1f2da37617572d90f10e938bd
Roy Osherove <roy@astrails.com>
2011-06-05 00:59:07 +0300

    tddgit: 1 failed of 3 with 1 pending.

---
+++ specs2.rb
@@ -0,0 +1,11 @@
+require 'rspec'
+
+describe "pencil" do
+  it "draws circles"
+  it "fails something first " do 
+    1.should == 2
+  end
+  it "fails something" do 
+    1.should == 2
+  end
+end
